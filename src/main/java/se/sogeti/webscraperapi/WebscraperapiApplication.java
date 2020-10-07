f4dcf14a90fb5bcd21013d1c9d71086738ddadac
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-10-07 13:57:51 +0200

    Initial commit

---
+++ src/main/java/se/sogeti/webscraperapi/WebscraperapiApplication.java
@@ -0,0 +1,13 @@
+package se.sogeti.webscraperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscraperapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebscraperapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sogeti/webscraperapi/WebscraperapiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sogeti.webscraperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
