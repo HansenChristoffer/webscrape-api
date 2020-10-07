5b5aed131175f386d6dd9e47dcb519cda5704b78
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-10-07 16:21:07 +0200

    Capitalized column names and definitions in Category

--- src/main/java/se/sogeti/webscraperapi/models/Category.java
+++ src/main/java/se/sogeti/webscraperapi/models/Category.java
@@ -13,10 +13,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "category_name", columnDefinition = "varchar(255)", nullable = false, unique = true)
+    @Column(name = "CATEGORY_NAME", columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
     private String name;
 
-    @Column(name = "category_href", columnDefinition = "varchar(255)", nullable = false, unique = true)
+    @Column(name = "CATEGORY_HREF", columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
     private String categoryHref;
 
     //@Column(name = "category_adverts")
@@ -45,4 +45,12 @@ public class Category implements Serializable {
     public void setCategoryHref(String categoryHref) {
         this.categoryHref = categoryHref;
     }
+
+    // public Collections<Advert> getAdverts() {
+    //     return adverts;
+    // }
+
+    // public void setCollections(Collections<Advert> adverts) {
+    //     this.adverts = adverts;
+    // }
 }
