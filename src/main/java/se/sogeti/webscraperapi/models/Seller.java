c38c0e8d4bfbedbeaec4e40717927aa7993a920d
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-10-09 18:46:20 +0200

    misc

--- src/main/java/se/sogeti/webscraperapi/models/Seller.java
+++ src/main/java/se/sogeti/webscraperapi/models/Seller.java
@@ -15,12 +15,11 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
-@JsonIdentityInfo(
-  generator = ObjectIdGenerators.PropertyGenerator.class, 
-  property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Seller implements Serializable {
 
-    public Seller() {}
+    public Seller() {
+    }
 
     public Seller(String name, String location, String registered, String sellerHref, Collection<Advert> adverts) {
         this.name = name;
