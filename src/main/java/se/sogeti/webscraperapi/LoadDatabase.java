4e789ee18d8669b48dac4b545453cef545e35cff
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-11-10 11:27:08 +0100

    Removed mockdata functions, they're not in use and is just causing issues when changes to entities occurs

--- src/main/java/se/sogeti/webscraperapi/LoadDatabase.java
+++ src/main/java/se/sogeti/webscraperapi/LoadDatabase.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import se.sogeti.webscraperapi.models.Advert;
-import se.sogeti.webscraperapi.models.Category;
-import se.sogeti.webscraperapi.models.Link;
-import se.sogeti.webscraperapi.models.Seller;
 import se.sogeti.webscraperapi.services.AdvertService;
 import se.sogeti.webscraperapi.services.CategoryService;
 import se.sogeti.webscraperapi.services.LinkService;
@@ -18,7 +14,6 @@ import se.sogeti.webscraperapi.services.SellerService;
 @Configuration
 public class LoadDatabase {
 
-  private final String preloadLoggerMsg = "<$> Preloading : ";
   private CategoryService categoryService;
   private SellerService sellerService;
   private AdvertService advertService;
@@ -35,45 +30,9 @@ public class LoadDatabase {
 
     return args -> {
       // Clear database
-      linkService.deleteAll();
-      sellerService.deleteAll();
-      // categoryService.deleteAll();
-      advertService.deleteAll();
-
-      // Links
-      // mockLinks();
-
-      // Adverts
-      // mockAdverts();
+      // linkService.deleteAll();
+      // sellerService.deleteAll();
+      // advertService.deleteAll();
     };
-  }
-
-  private void mockLinks() {
-    Link mockLink = new Link("https://www.mocklink.io/coolstuff");
-    Link mockLink1 = new Link("https://www.mocklink.io/pewpew");
-    Link mockLink2 = new Link("https://www.mocklink.io/cowcow");
-    Link mockLink3 = new Link("https://www.mocklink.io/bajs");
-    log.info("{}{}", preloadLoggerMsg, linkService.createLink(mockLink));
-    log.info("{}{}", preloadLoggerMsg, linkService.createLink(mockLink1));
-    log.info("{}{}", preloadLoggerMsg, linkService.createLink(mockLink2));
-    log.info("{}{}", preloadLoggerMsg, linkService.createLink(mockLink3));
-  }
-
-  private void mockAdverts() {
-    Category mockCategory = new Category("mockCategory", "categoryHref", true);
-    log.info("{}{}", preloadLoggerMsg, categoryService.createCategory(mockCategory));
-
-    Seller mockSeller = new Seller("mockSeller", "location", "sellerHref");
-    sellerService.createSeller(mockSeller);
-    log.info("{}{}", preloadLoggerMsg, sellerService.createSeller(mockSeller));
-
-    Advert mockAdvertOne = new Advert("mockAdvertOne", mockCategory.getName(), mockSeller.getName(), "description",
-        1000.0, "published", "98348748", "mockAdvertOne", "mockCondition", "mockBrand", "mockSize", "mockColor",
-        new byte[0]);
-    Advert mockAdvertTwo = new Advert("mockAdvertTwo", mockCategory.getName(), mockSeller.getName(), "description",
-        1000.0, "published", "834863423", "mockAdvertTwo", "mockCondition", "mockBrand", "mockSize", "mockColor",
-        new byte[0]);
-    log.info("{}{}", preloadLoggerMsg, advertService.createAdvert(mockAdvertOne));
-    log.info("{}{}", preloadLoggerMsg, advertService.createAdvert(mockAdvertTwo));
   }
 }
