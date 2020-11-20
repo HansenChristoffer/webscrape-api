7f916fedf0e75b649158ad961bc98844a3ae3b1e
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-11-20 16:04:10 +0100

    Added some log information for file structure checks

--- src/main/java/se/sogeti/webscraperapi/WebscraperapiApplication.java
+++ src/main/java/se/sogeti/webscraperapi/WebscraperapiApplication.java
@@ -5,9 +5,11 @@ import java.io.File;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
 import se.sogeti.webscraperapi.constants.Settings;
 
 @SpringBootApplication
+@Slf4j
 public class WebscraperapiApplication {
 
 	public static void main(String[] args) {
@@ -16,12 +18,17 @@ public class WebscraperapiApplication {
 	}
 
 	private static void checkFileStructures() {
+		log.info("File structure check commencing...");
+
 		File[] paths = { new File(Settings.BASE_SETTINGS_RELATIVE_PATH), new File(Settings.BASE_IMAGES_RELATIVE_PATH) };
 
 		for (File f : paths) {
 			if (!f.exists()) {
+				log.info("File [!exists] : {} -> building...", f);
 				f.mkdirs();
 			}
 		}
+
+		log.info("File structure check complete!");
 	}
 }
